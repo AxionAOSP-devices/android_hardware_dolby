2cd3b869546fe554526d30bf6c01656fcfbac3e0
zaraknyu <razenxizu@gmail.com>
2026-08-01 08:01:00 +0000

    dolby: Import odm and vendor properties
    Signed-off-by: zaraknyu <razenxizu@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,9 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Properties
+TARGET_ODM_PROP += $(DOLBY_PATH)/properties/odm.prop
+TARGET_VENDOR_PROP += $(DOLBY_PATH)/properties/vendor.prop
+
--- aidl-dolby.mk
+++ aidl-dolby.mk
@@ -39,3 +39,29 @@ PRODUCT_COPY_FILES += \
 # Overlays
 PRODUCT_PACKAGES += \
     DolbyFrameworksResCommon
+
+# Spatial Audio: optimize spatializer effect
+PRODUCT_VENDOR_PROPERTIES += \
+       audio.spatializer.effect.util_clamp_min=300
+
+# Spatial Audio: declare use of spatial audio
+PRODUCT_VENDOR_PROPERTIES += \
+       ro.audio.spatializer_enabled=true \
+       ro.audio.headtracking_enabled=true \
+       ro.audio.spatializer_transaural_enabled_default=false \
+       ro.audio.stereo_spatialization_enabled=true \
+       persist.vendor.audio.spatializer.speaker_enabled=true
+
+# Codec2 Props
+PRODUCT_VENDOR_PROPERTIES += \
+    vendor.audio.c2.preferred=true \
+    debug.c2.use_dmabufheaps=1 \
+    vendor.qc2audio.suspend.enabled=true \
+    vendor.qc2audio.per_frame.flac.dec.enabled=true
+
+# Dolby Props
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.dolby.dax.version=DAX3_3.7.0.8_r1 \
+    vendor.audio.dolby.ds2.hardbypass=false \
+    vendor.audio.dolby.ds2.enabled=false
+
