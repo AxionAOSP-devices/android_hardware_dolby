abec20a83a465c1fdc22cd17d2323c7d8a7094e5
zaraknyu <razenxizu@gmail.com>
2026-08-01 08:13:36 +0000

    dolby: Label dolby selinux denials
    Signed-off-by: zaraknyu <razenxizu@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,3 +7,5 @@
 TARGET_ODM_PROP += $(DOLBY_PATH)/properties/odm.prop
 TARGET_VENDOR_PROP += $(DOLBY_PATH)/properties/vendor.prop
 
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DOLBY_PATH)/sepolicy/vendor
